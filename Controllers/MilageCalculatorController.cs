310adb9e032e52e801eb8c040db41cd7af8dae9b
sheetWeaver <admin@pixelsheets.com>
2021-05-24 11:08:53 -0700

    First commit.

---
+++ Controllers/MilageCalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AlaskaAirMilesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MilageCalculatorController : ControllerBase
+    {
+        [HttpGet("{city}")]
+        public ActionResult<MilageCalculator> Get(string city)
+        {
+            
+        }
+    }
+}
---
+++ MilageCalculator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AlaskaAirMilesAPI
+{
+    public class MilageCalculator
+    {
+        public string City { get; set; }
+        public int Miles { get; set; }
+
+        public int FlightTime { get; set; }
+    }
+}
